6f0e3b1223de691c8c49a63e20bc74e8ff61fa01
eull0004 <nathan.eullaffroy@etudiant.univ-reims.fr>
2023-03-12 15:03:00 +0100

    Ajout des méthodes d'instance dans la classe Maze + test des méthodes d'instance réussis

--- Maze.py
+++ Maze.py
@@ -12,14 +12,13 @@ class Maze:
         Constructeur d'un labyrinthe de height cellules de haut,
         de width cellules de large et d'un empty ayant comme valeur un booléen
         Les voisinages sont initialisés à des ensembles vides
-        Si empty = True, contruction d'une grille où chaque cellule a pour voisines celles qu'elles touchent donc aucun mur n'est généré
-        Et si empty = False, construction d'une grille où aucune cellule n’a de voisines donc tous les murs sont générés.
+        Si empty = True, contruction d'une grille où chaque cellule a pour voisines celles qui lui sont contigües donc aucun mur n'est généré
+        Et si empty = False, construction d'une grille où aucune cellule n’a de voisines donc tous les murs sont générés
         """
         self.height    = height
         self.width     = width
         self.neighbors = {(i,j): set() for i in range(height) for j in range (width)}
-        self.empty = empty
-        
+        self.empty     = empty
         if empty == True:
              for i in range(1,height):
                 for j in range(width):
@@ -38,7 +37,7 @@ class Maze:
                         self.neighbors[(j,i-1)].remove((j,i)) 
                     elif (j,i-1) in self.neighbors[(j,i)]:
                         self.neighbors[(j,i)].remove((j,i-1))
-        
+    
     def info(self):
         """
         **NE PAS MODIFIER CETTE MÉTHODE**
@@ -96,8 +95,108 @@ class Maze:
         txt += "━━━┛\n"
 
         return txt
+
+    """
+    Méthodes d'instance permettant d'ajouter un mur entre deux cellules c1 et c2 placées en paramètres
+    Version robuste permettant de tester la présence des sommets dans la grille
+    """
+    def add_wall(self, c1, c2):
+        # Facultatif : on teste si les sommets sont bien dans le labyrinthe
+        assert 0 <= c1[0] < self.height and \
+            0 <= c1[1] < self.width and \
+            0 <= c2[0] < self.height and \
+            0 <= c2[1] < self.width, \
+            f"Erreur lors de l'ajout d'un mur entre {c1} et {c2} : les coordonnées de sont pas compatibles avec les dimensions du labyrinthe"
+        # Ajout du mur
+        if c2 in self.neighbors[c1]:      # Si c2 est dans les voisines de c1
+            self.neighbors[c1].remove(c2) # on le retire
+        if c1 in self.neighbors[c2]:      # Si c3 est dans les voisines de c2
+            self.neighbors[c2].remove(c1) # on le retire
+
+    """
+    Méthode d'instance permettant d'obtenir la liste des cellules de la grille
+    afin de faciliter par la suite d'autres méthodes d'instance
+    """
+    def get_cells(self):
+        L = []
+        for i in range(self.height):
+            for j in range(self.width):
+                L.append((i,j))
+        return L
     
-    
-    def remove_wall(self, c1:tuple, c2:tuple):
+    """
+    Méthode d'instance permettant de retirer un mur entre deux cellules c1 et c2 placées en paramètre.
+    Mais donc de les ajouter dans leurs voisines respectives.
+    """
+    def remove_wall(self,c1,c2):
         self.neighbors[c1].add(c2)
         self.neighbors[c2].add(c1)
+
+    """
+    Méthode d'instance permettant d'obtenir la liste de tous les murs de la grille.
+    """
+    def get_walls(self):
+        L = []
+        for i in range(self.height):
+            for j in range(self.width):
+                c1 = (i,j)
+                c2 = (c1[0],c1[1]+1)
+                c3 = (c1[0]+1,c1[1])
+                if c2 in self.get_cells() and c2 not in self.neighbors[c1]:
+                    L.append((c1,c2))
+                elif c3 in self.get_cells() and c3 not in self.neighbors[c1]:
+                    L.append((c1,c3))
+        return L
+
+    """
+    Méthode d'instance permettant d'ajouter tous les murs possibles dans le labyrinthe.
+    """
+    def fill(self):
+        for i in range(self.width):
+            for j in range(self.height):
+                voisin_lst = []
+                for elt in self.neighbors[(i,j)]:
+                    voisin_lst.append(elt)
+                for k in range(len(voisin_lst)):
+                    self.neighbors[(i,j)].remove(voisin_lst[k])
+                    self.neighbors[voisin_lst[k]].remove((i,j))
+
+    """
+    Méthode d'instance permettant de retirer tous les murs du labyrinthe.
+    """
+    def empty_(self):
+         for i in range(1,self.height):
+                for j in range(self.width):
+                    self.neighbors[(i,j)].add((i-1,j)) 
+                    self.neighbors[(i-1,j)].add((i,j))
+                    self.neighbors[(j,i)].add((j,i-1)) 
+                    self.neighbors[(j,i-1)].add((j,i))
+
+    """
+    Méthode d'instance permettant d'obtenir la liste de toutes les cellules touchant la cellule c mise en paramètre
+    On ne prend pas en compte les murs. 
+    """
+    def get_contiguous_cells(self, c):
+        contigue = []
+        i = c[0]
+        j = c[1]
+        if i-1 >= 0:
+            contigue.append((i-1,j))
+        if i+1 < self.height:
+            contigue.append((i+1,j))
+        if j-1 >= 0:
+            contigue.append((i,j-1))
+        if j+1 < self.width:
+            contigue.append((i,j+1))
+        return contigue
+
+    """
+    Méthode d'instance permettant d'obtenir la liste de toutes les cellules atteignables depuis la cellule c mise en paramètre
+    C'est à dire les cellules touchant c n'étant pas séparées par un mur.
+    """
+    def get_reachable_cells(self, c):
+        access = []
+        for i in range(len(self.get_contiguous_cells(c))):
+            if c in self.neighbors[self.get_contiguous_cells(c)[i]] and self.get_contiguous_cells(c)[i] in self.neighbors[c]:
+                access.append(self.get_contiguous_cells(c)[i])
+        return access 
